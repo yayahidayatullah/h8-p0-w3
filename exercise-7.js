a8dbcdf0ab85218582c5becc75f2f0b2b21aec8b
days88 <yayahidayatullah@rocketmail.com>
2018-08-09 18:44:34 +0800

    Hitung jumlah kata

---
+++ exercise-7.js
@@ -0,0 +1,13 @@
+function hitungJumlahKata(kalimat) {
+    //kalimat dijadikan array
+    var ubahArray = kalimat.split(" ");
+    //hitung isi dari array
+    return ubahArray.length ;
+  }
+  
+  // TEST CASES
+  console.log(hitungJumlahKata('I have a dream')); // 4
+  console.log(hitungJumlahKata('Never eat shredded wheat or cake')); // 6
+  console.log(hitungJumlahKata('A song to sing')); // 4
+  console.log(hitungJumlahKata('I')); // 1
+  console.log(hitungJumlahKata('I believe I can code')); // 5
