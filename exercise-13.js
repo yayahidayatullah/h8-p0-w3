3795b4b2967803a3b8082e168fdc6a7cc0ea78ff
days88 <yayahidayatullah@rocketmail.com>
2018-08-10 20:57:07 +0800

    Target Terdekat

---
+++ exercise-13.js
@@ -0,0 +1,30 @@
+function targetTerdekat(arr) {
+    var posisiO = 0;
+    var posisiX = [];
+    for(var i =0 ; i < arr.length ;i++){
+        if(arr[i] === 'o'){
+            posisiO+= i;
+        }else if(arr[i] === 'x'){
+            posisiX.push(i);
+        }
+    }
+    // console.log(posisiX);
+    // return posisiO;
+    
+    if(posisiO < posisiX[0]){
+        return posisiX[0] - posisiO
+    }else if(posisiO > posisiX[0]){
+        var angkaTerakhir = posisiX.length-1
+        return posisiO - posisiX[angkaTerakhir]
+    }else{
+        return 0
+    }
+  }
+  
+  // TEST CASES
+  console.log(targetTerdekat([' ', ' ', 'o', ' ', ' ', 'x', ' ', 'x'])); // 3
+  console.log(targetTerdekat(['o', ' ', ' ', ' ', 'x', 'x', 'x'])); // 4
+  console.log(targetTerdekat(['x', ' ', ' ', ' ', 'x', 'x', 'o', ' '])); // 1
+  console.log(targetTerdekat([' ', ' ', 'o', ' '])); // 0
+  console.log(targetTerdekat([' ', 'o', ' ', 'x', 'x', ' ', ' ', 'x'])); // 2
+  console.log(targetTerdekat([' ', 'x', ' ', 'x', 'x', 'o', ' ', 'x'])); // 2
